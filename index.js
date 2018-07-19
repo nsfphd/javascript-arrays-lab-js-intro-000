7c3867bb953779b61c8725df6cb6e2771afaa1c8
Noelle Foster <uufarmgirl@gmail.com>
2018-07-19 18:36:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your functions and code here
+function destructivelyAppendKitten(myKitten){
+
+}
+
+function destructivelyPrependKitten(myKitten){
+
+}
+
+function destructivelyRemoveLastKitten(){
+
+}
+
+function destructivelyRemoveFirstKitten(){
+
+}
+
+function appendKitten(myKitten){
+
+}
+
+function prependKitten(myKitten){
+
+}
+
+function removeLastKitten(){
+
+}
+
+function removeFirstKitten(){
+  
+}
